9a0ec86483a7cc1b4e9c42d2237a31583efca517
Abdullah Al Masud <masudctg00@gmail.com>
2022-06-26 01:55:57 +0600

    button design done

--- script.js
+++ script.js
@@ -7,6 +7,14 @@ document.getElementById('layout1').addEventListener('click', function () {
     let container2 = document.getElementById('container2');
     let container3 = document.getElementById('container3');
 
+    document.getElementById('layout1').style.backgroundColor = '#606060';
+    document.getElementById('layout2').style.backgroundColor = '';
+    document.getElementById('layout3').style.backgroundColor = '';
+
+    document.getElementById('layout1').style.color = 'white';
+    document.getElementById('layout2').style.color = '';
+    document.getElementById('layout3').style.color = '';
+
     container1.style.display = 'block'
     container2.style.display = 'none'
     container3.style.display = 'none'
@@ -17,6 +25,14 @@ document.getElementById('layout2').addEventListener('click', function () {
     let container2 = document.getElementById('container2');
     let container3 = document.getElementById('container3');
 
+    document.getElementById('layout1').style.backgroundColor = '';
+    document.getElementById('layout2').style.backgroundColor = '#606060';
+    document.getElementById('layout3').style.backgroundColor = '';
+
+    document.getElementById('layout1').style.color = '';
+    document.getElementById('layout2').style.color = 'white';
+    document.getElementById('layout3').style.color = '';
+
     container1.style.display = 'none'
     container2.style.display = 'grid'
     container3.style.display = 'none'
@@ -26,6 +42,14 @@ document.getElementById('layout3').addEventListener('click', function () {
     let container1 = document.getElementById('container1');
     let container2 = document.getElementById('container2');
     let container3 = document.getElementById('container3');
+
+    document.getElementById('layout1').style.backgroundColor = '';
+    document.getElementById('layout2').style.backgroundColor = '';
+    document.getElementById('layout3').style.backgroundColor = '#606060';
+
+    document.getElementById('layout1').style.color = '';
+    document.getElementById('layout2').style.color = '';
+    document.getElementById('layout3').style.color = 'white';
 
     container1.style.display = 'none'
     container2.style.display = 'none'
